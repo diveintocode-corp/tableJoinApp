c0c7b295518a267f5b65e26da5823c997d5dd6ed
keikamiguchi <kei.kamiguchi517@gmail.com>
2022-07-08 14:21:39 +0900

    fix

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -109,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Tokyo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
