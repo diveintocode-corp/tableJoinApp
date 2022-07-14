f7cdb5caa0c375fb785a301052adebcb070955f4
keikamiguchi <kei.kamiguchi517@gmail.com>
2022-07-14 13:11:29 +0900

    update genericview

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,26 +1,9 @@
 from django.urls import path
-from django.views.generic import ListView, CreateView, DetailView
-from .forms import CustomUserCreationForm
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
+from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', ListView.as_view(
-        template_name='accounts/index.html',
-        model=User,
-        context_object_name='users',
-    ), name='index'),
-    path('signup/', CreateView.as_view(
-        template_name='accounts/signup.html',
-        form_class=CustomUserCreationForm,
-        success_url='/accounts',
-    ), name='signup'),
-    path('user_detail/<int:pk>/', DetailView.as_view(
-        model=User,
-        template_name='accounts/detail.html',
-        context_object_name='user',
-    ), name='detail')
+    path('', views.UserList.as_view(), name='index'),
+    path('signup/', views.UserCreateView.as_view(), name='signup'),
+    path('detail/<int:pk>/', views.UserDetail.as_view(), name='detail')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView
+from .forms import CustomUserCreationForm
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserList(ListView):
+    model = User
+    template_name = "accounts/index.html"
+    context_object_name = 'users'
+
+
+class UserCreateView(CreateView):
+    template_name = 'accounts/signup.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('accounts:index')
+
+
+class UserDetail(DetailView):
+    model = User
+    template_name = "accounts/detail.html"
+    context_object_name = 'user'
